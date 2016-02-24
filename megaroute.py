e65799a83f5d70e0459284837085f917ae623c6a
fleabite <fleabite@fleabites-MacBook-Air.local>
2016-02-23 18:18:29 -0800

    Autocommit

--- megaroute.py
+++ megaroute.py
@@ -12,4 +12,20 @@ class MegaRoutes:
     def __iter__(self):
         return iter(self.route_list)
 
+    def __len__(self):
+        return len(self.route_list)
+
+    def __repr__(self):
+        return_string = ""
+        for route in self.route_list:
+            return_string += str(route)
+
+        return return_string
+
+
+
+    def AddRouteSchedule(self, fromcity, tocity, day, time):
+        """check if route already exists in list, else add it""
+        self.index
+
 
