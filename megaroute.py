c6bc2aa3931ff625d6cb452c1e0f297161b79120
fleabite <fleabite@fleabites-MacBook-Air.local>
2016-02-23 17:53:52 -0800

    Autocommit

---
+++ megaroute.py
@@ -0,0 +1,15 @@
+
+import ipdb
+
+
+class MegaRoute:
+    def __init__(self):
+        self.route_list = []
+
+    def AddRoute(self, fromcity, tocity):
+        self.route_list.append([fromcity, tocity])
+
+    def __iter__(self):
+        return iter(self.route_list)
+
+
