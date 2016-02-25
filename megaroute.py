603a4dda4d19a6070160d240922883bed549f1f7
fleabite <fleabite@fleabites-MacBook-Air.local>
2016-02-24 23:53:20 -0800

    Autocommit

--- megaroute.py
+++ megaroute.py
@@ -1,13 +1,39 @@
 
 import ipdb
 
+class MegaRoute:
+    def __init__(self, fromcity, tocity, day = "", time = ""):
+        self.fromcity = fromcity
+        self.tocity = tocity
+
+    def IsRoute(self, fromcity, tocity):
+        if self.fromcity == fromcity and self.tocity == tocity:
+            return True
+
+        return False
+
+    def __repr__(self):
+        return "%s, %s" % (self.fromcity, self.tocity)
+
+
+
 
 class MegaRoutes:
     def __init__(self):
         self.route_list = []
 
     def AddRoute(self, fromcity, tocity):
-        self.route_list.append([fromcity, tocity])
+        self.route_list.append(MegaRoute(fromcity, tocity))
+
+    def ViewRoute(self, fromcity, tocity):
+        for index in range(0, len(self.route_list)):
+            if self.route_list[index].IsRoute(fromcity, tocity):
+            //if self.route_list[index][0] == fromcity and self.route_list[index][1] == tocity:
+                return self.route_list[index]
+        
+        return None
+    
+
 
     def __iter__(self):
         return iter(self.route_list)
@@ -26,6 +52,18 @@ class MegaRoutes:
 
     def AddRouteSchedule(self, fromcity, tocity, day, time):
         """check if route already exists in list, else add it"""
-        self.index
+
+        need_to_add = True
+
+        for index in range(0, len(self.route_list)):
+            if self.route_list[index][0] == fromcity and self.route_list[index][1] == tocity:
+                need_to_add = False
+                break
+
+        if need_to_add:
+            self.AddRoute(fromcity, tocity)
+
+
+
 
 
